33d2a5ff56ea81334de3d529697ca9980300a0c2
Alexey Taranov <alexey.tarano0v@gmail.com>
2021-08-06 23:26:44 +0300

    Different rects for correct work with click references in property with SRD

--- Editor/SRDDrawer.cs
+++ Editor/SRDDrawer.cs
@@ -7,26 +7,30 @@ namespace SRD.Editor
     [CustomPropertyDrawer(typeof(SRDAttribute))]
     public class SRDDrawer : PropertyDrawer
     {
+        private const int DropdownHeight = 20;
         private SerializedPropertyInfo _serializedPropertyInfo;
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
+            if (property.propertyType == SerializedPropertyType.ManagedReference)
+            {
+                return EditorGUI.GetPropertyHeight(property, label, true) + DropdownHeight;
+            }
             return EditorGUI.GetPropertyHeight(property, label, true);
         }
 
         public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
         {
             EditorGUI.BeginProperty(rect, label, property);
-
-            // Don't make child fields be indented
+            
             var indent = EditorGUI.indentLevel;
             EditorGUI.indentLevel = 0;
-
-
+            
             if (property.propertyType == SerializedPropertyType.ManagedReference)
             {
-                DrawSRDProperty(rect, property, label);
-                EditorGUI.PropertyField(rect, property, label, true);
+                var rects = SplitRect();
+                DrawSRDTypeDropdown(rects.dropdownRect, property, label);
+                EditorGUI.PropertyField(rects.propertyRect, property, label, true);
             }
             else
             {
@@ -34,16 +38,25 @@ namespace SRD.Editor
             }
 
             EditorGUI.indentLevel = indent;
-
             EditorGUI.EndProperty();
+
+            (Rect dropdownRect, Rect propertyRect) SplitRect()
+            {
+                var dropdownRect = new Rect(rect.x, rect.y, rect.width, DropdownHeight);
+                var propertyRect = new Rect(rect.x, rect.y + DropdownHeight, rect.width, rect.height - DropdownHeight);
+                return (dropdownRect, propertyRect);
+            }
         }
 
-        void DrawSRDProperty(Rect rect, SerializedProperty property, GUIContent label)
+        void DrawSRDTypeDropdown(Rect rect, SerializedProperty property, GUIContent label)
         {
             _serializedPropertyInfo ??= new SerializedPropertyInfo(property);
             var selectedIndex = _serializedPropertyInfo.GetIndexAssignedTypeOfProperty(property);
-            var newIndex = EditorGUI.Popup(rect, label.text, selectedIndex,
+            var oldText = label.text;
+            var srdLabel = $"[SRD] {label.text}";
+            var newIndex = EditorGUI.Popup(rect, srdLabel, selectedIndex,
                 _serializedPropertyInfo.AssignableTypeNames);
+            label.text = oldText;
             if (selectedIndex != newIndex)
             {
                 Undo.RecordObject(property.serializedObject.targetObject, "Dropdown");
---
+++ Sample/TestScene/ColliderShape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class ColliderShape : IShape
+{
+    [SerializeField]
+    private Collider _collider;
+        
+    public float GetArea()
+    {
+        var size = _collider.bounds.size;
+        return size.x * size.y * size.z;
+    }
+}
