519e19c8bda7665fe9038dd0253eff70caa241f1
Alexey Taranov <alexey.taranov0@gmail.com>
2022-04-01 20:13:03 +0300

    0.0.3 - Issue 1: Find private field in base class. Editor asmdef - only editor platform

--- Editor/SerializedPropertyInfo.cs
+++ Editor/SerializedPropertyInfo.cs
@@ -8,14 +8,12 @@ namespace SRD.Editor
 {
     public class SerializedPropertyInfo
     {
+        private const string ArrayPropertySubstring = ".Array.data[";
+
         public readonly string[] AssignableTypeNames;
-        public readonly Type[] AssignableTypes;
+
+        private readonly Type[] _assignableTypes;
         private readonly FieldInfo _fieldInfo;
-
-        public static bool IsArrayProperty(SerializedProperty property) =>
-            property.propertyPath.Contains(arrayPropertySubstring);
-
-        private static string arrayPropertySubstring = ".Array.data[";
 
         public SerializedPropertyInfo(SerializedProperty property)
         {
@@ -23,26 +21,79 @@ namespace SRD.Editor
             var propertyPath = property.propertyPath;
             if (IsArrayProperty(property))
             {
-                var startIndexArrayPropertyPath = property.propertyPath.IndexOf(arrayPropertySubstring);
+                var startIndexArrayPropertyPath = property.propertyPath.IndexOf(ArrayPropertySubstring);
                 propertyPath = propertyPath.Remove(startIndexArrayPropertyPath);
             }
 
-            _fieldInfo = serializedObjectType.GetField(propertyPath,
-                BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+            _fieldInfo = GetFieldFromHierarchyToBaseType(serializedObjectType);
             var fieldType = ReflectionUtils.ExtractReferenceFieldTypeFromSerializedProperty(property);
             var allTypes = ReflectionUtils.GetAllTypesInCurrentDomain();
-            AssignableTypes = ReflectionUtils.GetFinalAssignableTypes(fieldType, allTypes);
-            AssignableTypeNames = AssignableTypes.Select(type => type.Name).ToArray();
+            _assignableTypes = ReflectionUtils.GetFinalAssignableTypes(fieldType, allTypes);
+            AssignableTypeNames = _assignableTypes.Select(type => type.Name).ToArray();
             if (!fieldType.IsValueType)
             {
-                var typesWithNull = new List<Type> {null};
-                typesWithNull.AddRange(AssignableTypes);
-                AssignableTypes = typesWithNull.ToArray();
-                var typeNamesWithNull = new List<string> {"null"};
+                var typesWithNull = new List<Type> { null };
+                typesWithNull.AddRange(_assignableTypes);
+                _assignableTypes = typesWithNull.ToArray();
+                var typeNamesWithNull = new List<string> { "null" };
                 typeNamesWithNull.AddRange(AssignableTypeNames);
                 AssignableTypeNames = typeNamesWithNull.ToArray();
             }
+
+            FieldInfo GetFieldFromHierarchyToBaseType(Type type)
+            {
+                var field = type.GetField(propertyPath,
+                    BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+                if (field == null)
+                {
+                    var baseType = type.BaseType;
+                    if (baseType != null)
+                    {
+                        return GetFieldFromHierarchyToBaseType(baseType);
+                    }
+                }
+
+                return field;
+            }
         }
+
+        public bool CanShowSRD() => _assignableTypes.Any() && _fieldInfo != null;
+
+        public int GetIndexAssignedTypeOfProperty(SerializedProperty property)
+        {
+            var targetObject = property.serializedObject.targetObject;
+            var objectValue = _fieldInfo.GetValue(targetObject);
+            if (IsArrayProperty(property))
+            {
+                int index = GetIndexArrayElementProperty(property);
+                var objectsArray = (object[])objectValue;
+                objectValue = objectsArray[index];
+            }
+
+            if (objectValue is null) return 0;
+
+            return Array.IndexOf(_assignableTypes, objectValue.GetType());
+        }
+
+        public Type GetTypeAtIndex(int index) => _assignableTypes[index];
+
+        public void ApplyValueToProperty(object value, SerializedProperty property)
+        {
+            var targetObject = property.serializedObject.targetObject;
+            if (IsArrayProperty(property))
+            {
+                var arrayFieldObjects = _fieldInfo.GetValue(targetObject);
+                int index = GetIndexArrayElementProperty(property);
+                var objectsArray = (object[])arrayFieldObjects;
+                objectsArray[index] = value;
+            }
+            else
+            {
+                _fieldInfo.SetValue(targetObject, value);
+            }
+        }
+
+        bool IsArrayProperty(SerializedProperty property) => property.propertyPath.Contains(ArrayPropertySubstring);
 
         private int GetIndexArrayElementProperty(SerializedProperty property)
         {
@@ -56,38 +107,6 @@ namespace SRD.Editor
             }
 
             return index;
-        }
-
-        public int GetIndexAssignedTypeOfProperty(SerializedProperty property)
-        {
-            var targetObject = property.serializedObject.targetObject;
-            var objectValue = _fieldInfo.GetValue(targetObject);
-            if (IsArrayProperty(property))
-            {
-                int index = GetIndexArrayElementProperty(property);
-                var objectsArray = (object[]) objectValue;
-                objectValue = objectsArray[index];
-            }
-
-            if (objectValue is null) return 0;
-
-            return Array.IndexOf(AssignableTypes, objectValue.GetType());
-        }
-
-        public void ApplyValueToProperty(object value, SerializedProperty property)
-        {
-            var targetObject = property.serializedObject.targetObject;
-            if (IsArrayProperty(property))
-            {
-                var arrayFieldObjects = _fieldInfo.GetValue(targetObject);
-                int index = GetIndexArrayElementProperty(property);
-                var objectsArray = (object[]) arrayFieldObjects;
-                objectsArray[index] = value;
-            }
-            else
-            {
-                _fieldInfo.SetValue(targetObject, value);
-            }
         }
     }
 }
