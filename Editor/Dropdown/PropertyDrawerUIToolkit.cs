6c37756127e968268998ddcbc02d729fb7200d65
Alexey Taranov <alexey.taranov0@gmail.com>
2026-01-14 22:34:22 +0400

    Refactored - UI Toolkit implementation removed local functions

--- Editor/Dropdown/PropertyDrawerUIToolkit.cs
+++ Editor/Dropdown/PropertyDrawerUIToolkit.cs
@@ -20,6 +20,12 @@ namespace SerializeReferenceDropdown.Editor.Dropdown
         private readonly SerializedProperty property;
         private readonly VisualElement root;
         private readonly IReadOnlyList<Type> assignableTypes;
+        private readonly string assetPath;
+
+        private Button selectTypeButton;
+        private Button openSourceFIleButton;
+        private Button showSearchToolButton;
+        private Button fixCrossRefButton;
 
         private StyleColor defaultSelectTypeTextColor;
 
@@ -32,12 +38,21 @@ namespace SerializeReferenceDropdown.Editor.Dropdown
 
         private Action _pingSelf;
 
+        private string propertyPath => property.propertyPath;
+        private Object targetObject => property.serializedObject.targetObject;
+
         public PropertyDrawerUIToolkit(SerializedProperty property, IReadOnlyList<Type> assignableTypes,
             VisualElement root)
         {
             this.property = property;
             this.assignableTypes = assignableTypes;
             this.root = root;
+            assetPath = AssetDatabase.GetAssetPath(targetObject);
+            if (string.IsNullOrEmpty(assetPath))
+            {
+                assetPath = PrefabUtility.GetPrefabAssetPathOfNearestInstanceRoot(targetObject);
+            }
+
             AddSelfToAllProperties();
         }
 
@@ -72,7 +87,7 @@ namespace SerializeReferenceDropdown.Editor.Dropdown
             Selection.activeObject = selectionObject;
             pingObject = selectionObject;
             pingRefId = refId;
-            PropertyDrawerUIToolkit.PingAll();
+            PingAll();
 
             EditorApplication.delayCall += () =>
             {
@@ -82,7 +97,7 @@ namespace SerializeReferenceDropdown.Editor.Dropdown
             };
         }
 
-        public static void PingAll()
+        private static void PingAll()
         {
             foreach (var pair in _allPropertyDrawers)
             {
@@ -105,17 +120,19 @@ namespace SerializeReferenceDropdown.Editor.Dropdown
             root.Add(visualTreeAsset.Instantiate());
             var propertyField = root.Q<PropertyField>();
             propertyField.BindProperty(property);
-            var propertyPath = property.propertyPath;
-            var targetObject = property.serializedObject.targetObject;
 
-            var selectTypeButton = root.Q<Button>("type-select");
-            selectTypeButton.clickable.clicked += ShowDropdown;
+            selectTypeButton = root.Q<Button>("type-select");
+            selectTypeButton.clickable.clicked += () =>
+            {
+                var propertyRect = propertyField.contentRect;
+                ShowDropdown(propertyRect);
+            };
             defaultSelectTypeTextColor = selectTypeButton.style.color;
 
-            var fixCrossRefButton = root.Q<Button>("fix-cross-references");
+            fixCrossRefButton = root.Q<Button>("fix-cross-references");
             fixCrossRefButton.clickable.clicked += () => { PropertyDrawerCrossReferences.FixCrossReference(property); };
 
-            var openSourceFIleButton = root.Q<Button>("open-source-file");
+            openSourceFIleButton = root.Q<Button>("open-source-file");
             openSourceFIleButton.SetDisplayElement(false);
             openSourceFIleButton.clicked += () =>
             {
@@ -123,13 +140,7 @@ namespace SerializeReferenceDropdown.Editor.Dropdown
             };
 
             var modifyDirectType = root.Q<Button>("modify-direct-type");
-            var assetPath = AssetDatabase.GetAssetPath(targetObject);
-            if (string.IsNullOrEmpty(assetPath))
-            {
-                assetPath = PrefabUtility.GetPrefabAssetPathOfNearestInstanceRoot(targetObject);
-            }
-
-            var showSearchToolButton = root.Q<Button>("show-search-tool");
+            showSearchToolButton = root.Q<Button>("show-search-tool");
 
             var canModifyDirectType = string.IsNullOrEmpty(assetPath) == false;
             modifyDirectType.SetDisplayElement(canModifyDirectType);
@@ -144,7 +155,7 @@ namespace SerializeReferenceDropdown.Editor.Dropdown
 
             root.TrackSerializedObjectValue(property.serializedObject, RefreshDropdown);
             RefreshDropdown(property.serializedObject);
-            
+
             var needPingOnFirstShowPropertyDrawer =
                 previousSelection != pingObject &&
                 previousSelection != null &&
@@ -156,149 +167,147 @@ namespace SerializeReferenceDropdown.Editor.Dropdown
             }
 
             _pingSelf = PingNow;
+        }
 
-            void PingNow()
-            {
-                if (pingObject == property.serializedObject.targetObject &&
-                    property.managedReferenceId == pingRefId)
+        private void ShowDropdown(Rect propertyRect)
+        {
+            var dropdown = new SerializeReferenceAdvancedDropdown(new AdvancedDropdownState(),
+                assignableTypes,
+                type =>
                 {
-                    PingImpl();
-                }
+                    PropertyDrawerTypesUtils.WriteNewInstanceByType(type, property, propertyRect,
+                        registerUndo: true);
+                });
+            var buttonMatrix = selectTypeButton.worldTransform;
+            var position = new Vector3(buttonMatrix.m03, buttonMatrix.m13, buttonMatrix.m23);
+            var buttonRect = new Rect(position, selectTypeButton.contentRect.size);
+            dropdown.Show(buttonRect);
+        }
+
+        private void RefreshDropdown(SerializedObject so)
+        {
+            if (so == null)
+            {
+                return;
             }
 
-            void PingImpl()
+            var prop = so.FindProperty(propertyPath);
+            var selectedType = TypeUtils.ExtractTypeFromString(prop.managedReferenceFullTypename);
+            var selectedTypeName = PropertyDrawerTypesUtils.GetTypeName(selectedType);
+            var tooltipText = $"Type Full Name: {selectedType?.FullName}";
+            var isMissingType =
+                PropertyDrawerTypesUtils.TryGetMissingType(property, assetPath, out var missingType);
+            if (isMissingType)
             {
-                var rootElement = root.Q<VisualElement>("root");
-                var pingStyle = "ping-serialize-reference";
-                if (rootElement.ClassListContains(pingStyle) == false)
-                {
-                    rootElement.AddToClassList(pingStyle);
-                    rootElement.RegisterCallback<TransitionEndEvent>(TransitionEndEventOnce);
-                }
-
-                void TransitionEndEventOnce(TransitionEndEvent evt)
-                {
-                    rootElement.RemoveFromClassList(pingStyle);
-                    rootElement.UnregisterCallback<TransitionEndEvent>(TransitionEndEventOnce);
-                }
+                selectedTypeName = $"MISSING TYPE: {missingType.className}";
+                tooltipText = missingType.GetDetailData();
+                selectTypeButton.AddToClassList("error-bg");
+            }
+            else
+            {
+                selectTypeButton.RemoveFromClassList("error-bg");
             }
 
-            void ShowDropdown()
+            selectTypeButton.text = selectedTypeName;
+            selectTypeButton.tooltip = tooltipText;
+            selectTypeButton.style.color = defaultSelectTypeTextColor;
+
+            openSourceFIleButton.SetDisplayElement(property.managedReferenceValue != null);
+
+            var activeSearchTool = SerializeReferenceToolsUserPreferences.GetOrLoadSettings().EnableSearchTool;
+            showSearchToolButton.SetDisplayElement(selectedType != null && activeSearchTool);
+            fixCrossRefButton.SetDisplayElement(false);
+
+            if (PropertyDrawerCrossReferences.IsHaveSameOtherSerializeReference(property, out var isNewElement))
             {
-                var propertyRect = propertyField.contentRect;
-                var dropdown = new SerializeReferenceAdvancedDropdown(new AdvancedDropdownState(),
-                    assignableTypes,
-                    type =>
+                fixCrossRefButton.SetDisplayElement(true);
+                var color = PropertyDrawerCrossReferences.GetColorForEqualSerializeReference(property);
+                selectTypeButton.style.color = color;
+                if (isNewElement)
+                {
+                    //https://github.com/AlexeyTaranov/SerializeReferenceDropdown/issues/49
+                    //HACK: For correct refresh duplicate references need to call some refresh inspector
+                    RefreshInspector();
+
+                    void RefreshInspector()
                     {
-                        PropertyDrawerTypesUtils.WriteNewInstanceByType(type, property, propertyRect,
-                            registerUndo: true);
-                    });
-                var buttonMatrix = selectTypeButton.worldTransform;
-                var position = new Vector3(buttonMatrix.m03, buttonMatrix.m13, buttonMatrix.m23);
-                var buttonRect = new Rect(position, selectTypeButton.contentRect.size);
-                dropdown.Show(buttonRect);
-            }
-
-            void RefreshDropdown(SerializedObject so)
-            {
-                if (so == null)
-                {
-                    return;
-                }
-
-                var prop = so.FindProperty(propertyPath);
-                var selectedType = TypeUtils.ExtractTypeFromString(prop.managedReferenceFullTypename);
-                var selectedTypeName = PropertyDrawerTypesUtils.GetTypeName(selectedType);
-                var tooltipText = $"Type Full Name: {selectedType?.FullName}";
-                var isMissingType =
-                    PropertyDrawerTypesUtils.TryGetMissingType(property, assetPath, out var missingType);
-                if (isMissingType)
-                {
-                    selectedTypeName = $"MISSING TYPE: {missingType.className}";
-                    tooltipText = missingType.GetDetailData();
-                    selectTypeButton.AddToClassList("error-bg");
-                }
-                else
-                {
-                    selectTypeButton.RemoveFromClassList("error-bg");
-                }
-
-                selectTypeButton.text = selectedTypeName;
-                selectTypeButton.tooltip = tooltipText;
-                selectTypeButton.style.color = defaultSelectTypeTextColor;
-
-                openSourceFIleButton.SetDisplayElement(property.managedReferenceValue != null);
-
-                var activeSearchTool = SerializeReferenceToolsUserPreferences.GetOrLoadSettings().EnableSearchTool;
-                showSearchToolButton.SetDisplayElement(selectedType != null && activeSearchTool);
-                fixCrossRefButton.SetDisplayElement(false);
-                modifyDirectType.SetDisplayElement(isMissingType || canModifyDirectType);
-
-                if (PropertyDrawerCrossReferences.IsHaveSameOtherSerializeReference(property, out var isNewElement))
-                {
-                    fixCrossRefButton.SetDisplayElement(true);
-                    var color = PropertyDrawerCrossReferences.GetColorForEqualSerializeReference(property);
-                    selectTypeButton.style.color = color;
-                    if (isNewElement)
-                    {
-                        //https://github.com/AlexeyTaranov/SerializeReferenceDropdown/issues/49
-                        //HACK: For correct refresh duplicate references need to call some refresh inspector
-                        RefreshInspector();
-
-                        void RefreshInspector()
-                        {
-                            var savedValue = property.managedReferenceValue;
-                            property.managedReferenceValue = null;
-                            property.serializedObject.ApplyModifiedPropertiesWithoutUndo();
-                            property.serializedObject.Update();
-                            property.managedReferenceValue = savedValue;
-                            property.serializedObject.ApplyModifiedPropertiesWithoutUndo();
-                            property.serializedObject.Update();
-                            PropertyDrawerGlobalCaches.DropCaches();
-                        }
+                        var savedValue = property.managedReferenceValue;
+                        property.managedReferenceValue = null;
+                        property.serializedObject.ApplyModifiedPropertiesWithoutUndo();
+                        property.serializedObject.Update();
+                        property.managedReferenceValue = savedValue;
+                        property.serializedObject.ApplyModifiedPropertiesWithoutUndo();
+                        property.serializedObject.Update();
+                        PropertyDrawerGlobalCaches.DropCaches();
                     }
                 }
             }
+        }
 
-            void ModifyDirectType()
+        private void ModifyDirectType()
+        {
+            var prop = property.serializedObject.FindProperty(propertyPath);
+            if (PropertyDrawerTypesUtils.TryGetMissingType(property, assetPath, out var missingType))
             {
-                var prop = property.serializedObject.FindProperty(propertyPath);
-                if (PropertyDrawerTypesUtils.TryGetMissingType(property, assetPath, out var missingType))
+                var typeData = new TypeData()
                 {
-                    var typeData = new TypeData()
-                    {
-                        AssemblyName = missingType.assemblyName,
-                        ClassName = missingType.className,
-                        Namespace = missingType.namespaceName
-                    };
-                    ShowModifyWindow(typeData, missingType.referenceId);
-                }
-                else if (prop.managedReferenceValue != null)
+                    AssemblyName = missingType.assemblyName,
+                    ClassName = missingType.className,
+                    Namespace = missingType.namespaceName
+                };
+                ShowModifyWindow(typeData, missingType.referenceId);
+            }
+            else if (prop.managedReferenceValue != null)
+            {
+                var type = prop.managedReferenceValue.GetType();
+                var typeData = new TypeData()
                 {
-                    var type = prop.managedReferenceValue.GetType();
-                    var typeData = new TypeData()
-                    {
-                        AssemblyName = type.Assembly.GetName().Name,
-                        ClassName = type.Name,
-                        Namespace = type.Namespace
-                    };
-                    ShowModifyWindow(typeData, property.managedReferenceId);
-                }
+                    AssemblyName = type.Assembly.GetName().Name,
+                    ClassName = type.Name,
+                    Namespace = type.Namespace
+                };
+                ShowModifyWindow(typeData, property.managedReferenceId);
+            }
 
-                void ShowModifyWindow(TypeData typeData, long refId)
-                {
-                    EditReferenceTypeWindow.ShowWindow(typeData,
-                        newData =>
+            void ShowModifyWindow(TypeData typeData, long refId)
+            {
+                EditReferenceTypeWindow.ShowWindow(typeData,
+                    newData =>
+                    {
+                        AssetDatabase.TryGetGUIDAndLocalFileIdentifier(targetObject, out var guid,
+                            out long localId);
+                        if (YamlEditUnityObject.TryModifyReferenceInFile(assetPath, localId, refId, newData))
                         {
-                            AssetDatabase.TryGetGUIDAndLocalFileIdentifier(targetObject, out var guid,
-                                out long localId);
-                            if (YamlEditUnityObject.TryModifyReferenceInFile(assetPath, localId, refId, newData))
-                            {
-                                AssetDatabase.ImportAsset(assetPath);
-                                PropertyDrawerGlobalCaches.DropCaches();
-                            }
-                        });
-                }
+                            AssetDatabase.ImportAsset(assetPath);
+                            PropertyDrawerGlobalCaches.DropCaches();
+                        }
+                    });
+            }
+        }
+        
+        private void PingNow()
+        {
+            if (pingObject == property.serializedObject.targetObject &&
+                property.managedReferenceId == pingRefId)
+            {
+                PingImpl();
+            }
+        }
+
+        private void PingImpl()
+        {
+            var rootElement = root.Q<VisualElement>("root");
+            var pingStyle = "ping-serialize-reference";
+            if (rootElement.ClassListContains(pingStyle) == false)
+            {
+                rootElement.AddToClassList(pingStyle);
+                rootElement.RegisterCallback<TransitionEndEvent>(TransitionEndEventOnce);
+            }
+
+            void TransitionEndEventOnce(TransitionEndEvent evt)
+            {
+                rootElement.RemoveFromClassList(pingStyle);
+                rootElement.UnregisterCallback<TransitionEndEvent>(TransitionEndEventOnce);
             }
         }
     }
