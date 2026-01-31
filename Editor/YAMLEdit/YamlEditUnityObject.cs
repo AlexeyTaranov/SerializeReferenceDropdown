ebfb395965fb235555c143632493177301b6b341
Alexey Taranov <alexey.taranov0@gmail.com>
2026-01-31 10:40:02 +0400

    Fix YamlDotNet for platofrm tests

--- Editor/YAMLEdit/YamlEditUnityObject.cs
+++ Editor/YAMLEdit/YamlEditUnityObject.cs
@@ -1,9 +1,9 @@
 using System;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using SerializeReferenceDropdown.Editor.Utils;
+using YamlDotNet;
 using YamlDotNet.Core;
 using YamlDotNet.RepresentationModel;
 
